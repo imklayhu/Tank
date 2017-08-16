843a045b51335638a549d625eec722dcac4ee05c
Klay-Clam <hellodearclam@outlook.com>
2017-08-16 14:12:55 +0800

    已经绘制出了地图背景，敌我的home，以及迷宫

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -1,15 +1,34 @@
 // 封装绘制 矩形的方法
-function drawRectangle(ctx,mapsize){
-    var fillStyle = mapsize.fillStyle;
-
-    ctx.save(); // 作用是什么？
-    ctx.beginPath();
-    ctx.fillStyle = fillStyle;
-    ctx.fillRect(startx,starty,endx,endy);
-    ctx.closePath();
+function drawRectangle(ctx,rectsize){
+    ctx.drawRect({
+        fillStyle: rectsize.fillStyle,
+        x: rectsize.startx, y: rectsize.starty,
+        width: rectsize.width,
+        height: rectsize.height,
+        fromCenter: false
+    })
 }
 
 // 封装绘制线条的方法
-function drawLine(){
+function drawLine(ctx,lineData){
+    ctx.drawLine({
+        strokeStyle: lineData.strokeStyle,
+        strokeWidth: 5,
+        x1: lineData.x1, y1: lineData.y1,
+        x2: lineData.x2, y2: lineData.y2
+      });
+}
 
+// 封装绘制坦克的方法
+function drawTank(ctx,tankData){
+    ctx.drawPolygon({
+        fillStyle: '#36c',
+        strokeStyle: '#f60',
+        strokeWidth: 2,
+        x: 100, y: 100,
+        radius: 20,
+        sides: 3,
+        concavity: -0.5,
+        rotate: 0
+      });
 }
--- public/javascripts/getmapdata.js
+++ public/javascripts/getmapdata.js
@@ -1,14 +1,12 @@
-var ctx_js = document.getElementById("map").getContext("2d");
 $(function(){
-    var ctx = ctx_js;
+    //var ctx = ctx_js;
     $.ajax({
         url:'/getmapdata',
         method: 'GET',
         dataType:'json',
         success:function(data){
-            console.log(data);
             // 解析并渲染数据
-            //renderData(data);
+            renderData(data);
         },
         error:function(err){
             console.log(err);
@@ -19,16 +17,52 @@ $(function(){
 // 地图数据请求成功的回调函数
 function renderData(data){
     var mapData = data[0];
+    console.log(mapData);
     //首先将地图的背景画出来
-    var mapsize = mapData.mapsize;
+    
+    var $mapbground = $("#map");
     // 在这里调用 绘制map背景的方法
+    var mapsize = mapData.mapsize;
+    drawRectangle($mapbground,mapsize);
+    // 在这里绘制tank home
+    var myhomeSize = mapData.mytankhome;
+    var enhomeSize = mapData.entankhome;
+    drawRectangle($mapbground,myhomeSize);
+    
+    for(var a_enhome of enhomeSize){
+        drawRectangle($mapbground,a_enhome);
+    }
+    // 在这里调用绘制迷宫的地图
+    // 调用解析数据函数
+    mazeData(mapData);
+    for(let i = 0;i < mazeData(mapData).startX.length;i++){
+        var lineData = {
+            strokeStyle: mazeData(mapData).strokeStyle,
+            x1: mazeData(mapData).startX[i],
+            y1: mazeData(mapData).startY[i],
+            x2: mazeData(mapData).endX[i],
+            y2: mazeData(mapData).endY[i]
+        };
+        drawLine($mapbground,lineData);
+    }
 
-    /*
-    *console.log(datajson);
-    *console.log(datajson.homequantity);
-    * 获取对象中的数据 的方式
-    *console.log(datajson.mapsize.height);
-    * 获取数组中数据的方式
-    *console.log(datajson.endy[0]);
-    */
+    // 在这里调用绘制我的坦克的方法
+    mytankData(mapData);
+    
+}
+
+// 迷宫的数据的解析
+function mazeData(mapData){
+    var startX = mapData.startx;
+    var startY = mapData.starty;
+    var endX = mapData.endx;
+    var endY = mapData.endy;
+    var strokeStyle = mapData.strokeStyle;
+
+    return {startX,startY,endX,endY,strokeStyle};
+}
+
+// 解析坦克的数据
+function mytankData(mapData){
+    var fillStyle = mapData.my
 }

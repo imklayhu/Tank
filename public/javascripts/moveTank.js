8b648f256c48f4b13ebf2e400683a11a9bffcd68
Clam <hellodearclam@outlook.com>
2017-08-20 10:46:39 +0800

    初步实现了坦克墙壁检测，还有bug，以及需要将这个方法做封装，冗余严重。

--- public/javascripts/moveTank.js
+++ public/javascripts/moveTank.js
@@ -3,81 +3,104 @@
 //         console.log("move up");
 //     }
 // } 
-function createTankMoveObject(ctx,tankData,mapData){
+function createTankMoveObject(ctx, tankData, mapData) {
     var m = new Object();
     //console.log(mapData["0"].mapsize.width);
     // m.mapData = mapData;
-    
+
     //console.log(m.mapData);
     m.ctx = ctx; // canvas 对象
     m.tankData = tankData; // 坦克的数据
     m.speed = 4;
     // 向上移动的方法
-    m.moveUp = function(){
+    m.moveUp = function () {
         console.log("move up");
         // 方向向上
         this.tankData.rotate = 0;
         // 地图边缘检测 以及地图的墙壁检测
-        if(this.tankData.y - this.tankData.radius < this.tankData.radius){
+        //var rgbaup = getPixelRgba(x,y);
+        if (this.tankData.y - this.tankData.radius < this.tankData.radius) {
             this.tankData.y = this.tankData.radius;
-            console.log(this.tankData.x,this.tankData.y);
+            console.log(this.tankData.x, this.tankData.y);
             // 坦克移动的时候重绘坦克和地图
             this.ctx.setLayer('moving', {
                 visible: false
             }).drawLayers();
             renderData(mapData);
             drawTank(this.ctx, this.tankData);
-        }else{
+        } else {
             // 墙壁检测
-            //if(getPixelRgba(this.tankData.x,this.tankData.y-this.tankData.radius-3) != "rgba(238,238,238,1)")
-            this.tankData.y = this.tankData.y - this.speed;
-            console.log(this.tankData.x,this.tankData.y);
-            var rgba = getPixelRgba(this.tankData.x,this.tankData.y-this.tankData.radius-3);
-            console.log("rgba: "+rgba);
-            // 坦克移动的时候重绘坦克和地图
-            this.ctx.setLayer('moving',{
-                visible: false
-            }).drawLayers();
-            renderData(mapData);
-            drawTank(this.ctx,this.tankData);
+            if (getPixelRgba(this.tankData.x, this.tankData.y - this.tankData.radius - 4) == "rgba(102,102,102,1)") {
+                this.tankData.y = this.tankData.y;
+                console.log("遇到墙壁：" + this.tankData.x, this.tankData.y);
+
+                // 坦克移动的时候重绘坦克和地图
+                this.ctx.setLayer('moving', {
+                    visible: false
+                }).drawLayers();
+                renderData(mapData);
+                drawTank(this.ctx, this.tankData);
+            } else {
+                this.tankData.y = this.tankData.y - this.speed;
+                console.log(this.tankData.x, this.tankData.y);
+                //var rgba = getPixelRgba(this.tankData.x,this.tankData.y-this.tankData.radius-3);
+                //console.log("rgba: "+rgba);
+                // 坦克移动的时候重绘坦克和地图
+                this.ctx.setLayer('moving', {
+                    visible: false
+                }).drawLayers();
+                renderData(mapData);
+                drawTank(this.ctx, this.tankData);
+            }
         }
-        
     };
     // 向右移动的方法
-    m.moveRight = function(){
+    m.moveRight = function () {
         console.log("move right");
         this.tankData.rotate = 90;
 
         // 地图边缘检测
-        if(700 - this.tankData.x <= this.tankData.radius){
-            this.tankData.x = 700-this.tankData.radius;
-            console.log(this.tankData.x,this.tankData.y);
+        if (700 - this.tankData.x <= this.tankData.radius) {
+            this.tankData.x = 700 - this.tankData.radius;
+            console.log(this.tankData.x, this.tankData.y);
             // 坦克移动的时候重绘坦克和地图
             this.ctx.setLayer('moving', {
                 visible: false
             }).drawLayers();
             renderData(mapData);
             drawTank(this.ctx, this.tankData);
-        }else{
-            this.tankData.x = this.tankData.x + this.speed;
-            console.log(this.tankData.x,this.tankData.y);
-            // 坦克移动的时候重绘坦克和地图
-            this.ctx.setLayer('moving',{
-                visible: false
-            }).drawLayers();
-            renderData(mapData);
-            drawTank(this.ctx,this.tankData);
+        } else {
+            if (getPixelRgba(this.tankData.x + this.tankData.radius + 4, this.tankData.y) == "rgba(102,102,102,1)") {
+                this.tankData.x = this.tankData.x;
+                console.log("遇到墙壁：" + this.tankData.x, this.tankData.y);
+
+                // 坦克移动的时候重绘坦克和地图
+                this.ctx.setLayer('moving', {
+                    visible: false
+                }).drawLayers();
+                renderData(mapData);
+                drawTank(this.ctx, this.tankData);
+            } else {
+                this.tankData.x = this.tankData.x + this.speed;
+                console.log(this.tankData.x, this.tankData.y);
+                // 坦克移动的时候重绘坦克和地图
+                this.ctx.setLayer('moving', {
+                    visible: false
+                }).drawLayers();
+                renderData(mapData);
+                drawTank(this.ctx, this.tankData);
+            }
         }
     }
     // 向下移动的方法
-    m.moveDown = function(){
+    m.moveDown = function () {
         console.log("move down");
         this.tankData.rotate = 180;
         // 边缘检测
         if (this.tankData.y + this.tankData.radius >= 500) {
             this.tankData.y = 500 - this.tankData.radius;
 
-            console.log(this.tankData.x,this.tankData.y);
+            console.log(this.tankData.x, this.tankData.y);
             // 坦克移动的时候重绘坦克和地图
             this.ctx.setLayer('moving', {
                 visible: false
@@ -85,23 +108,35 @@ function createTankMoveObject(ctx,tankData,mapData){
             renderData(mapData);
             drawTank(this.ctx, this.tankData);
         } else {
-            this.tankData.y = this.tankData.y + this.speed;
-            console.log(this.tankData.x,this.tankData.y);
-            // 坦克移动的时候重绘坦克和地图
-            this.ctx.setLayer('moving', {
-                visible: false
-            }).drawLayers();
-            renderData(mapData);
-            drawTank(this.ctx, this.tankData);
+            if (getPixelRgba(this.tankData.x, this.tankData.y + this.tankData.radius + 4) == "rgba(102,102,102,1)") {
+                this.tankData.y = this.tankData.y;
+                console.log("遇到墙壁：" + this.tankData.x, this.tankData.y);
+
+                // 坦克移动的时候重绘坦克和地图
+                this.ctx.setLayer('moving', {
+                    visible: false
+                }).drawLayers();
+                renderData(mapData);
+                drawTank(this.ctx, this.tankData);
+            }else{
+                this.tankData.y = this.tankData.y + this.speed;
+                console.log(this.tankData.x, this.tankData.y);
+                // 坦克移动的时候重绘坦克和地图
+                this.ctx.setLayer('moving', {
+                    visible: false
+                }).drawLayers();
+                renderData(mapData);
+                drawTank(this.ctx, this.tankData);
+            }
         }
     }
     // 向左移动的方法
-    m.moveLeft = function(){
+    m.moveLeft = function () {
         console.log("move left");
         this.tankData.rotate = 270;
         if (this.tankData.x - this.tankData.radius <= 0) {
             this.tankData.x = this.tankData.radius;
-            console.log(this.tankData.x,this.tankData.y);
+            console.log(this.tankData.x, this.tankData.y);
             // 坦克移动的时候重绘坦克和地图
             this.ctx.setLayer('moving', {
                 visible: false
@@ -109,25 +144,38 @@ function createTankMoveObject(ctx,tankData,mapData){
             renderData(mapData);
             drawTank(this.ctx, this.tankData);
         } else {
-            this.tankData.x = this.tankData.x - this.speed;
-            console.log(this.tankData.x,this.tankData.y);
-            // 坦克移动的时候重绘坦克和地图
-            this.ctx.setLayer('moving', {
-                visible: false
-            }).drawLayers();
-            renderData(mapData);
-            drawTank(this.ctx, this.tankData);
+            if(getPixelRgba(this.tankData.x - this.tankData.radius - 4, this.tankData.y) == "rgba(102,102,102,1)"){
+                this.tankData.x = this.tankData.x;
+                console.log("遇到墙壁：" + this.tankData.x, this.tankData.y);
+
+                // 坦克移动的时候重绘坦克和地图
+                this.ctx.setLayer('moving', {
+                    visible: false
+                }).drawLayers();
+                renderData(mapData);
+                drawTank(this.ctx, this.tankData);
+
+            }else{
+                this.tankData.x = this.tankData.x - this.speed;
+                console.log(this.tankData.x, this.tankData.y);
+                // 坦克移动的时候重绘坦克和地图
+                this.ctx.setLayer('moving', {
+                    visible: false
+                }).drawLayers();
+                renderData(mapData);
+                drawTank(this.ctx, this.tankData);
+            }
         }
     }
     return m;
 }
 
 // 键盘按键监听
-function listenKeyboard(tankMoveIntity){
-    $(document).keypress(function(event){
+function listenKeyboard(tankMoveIntity) {
+    $(document).keypress(function (event) {
         //console.log(event.keyCode);
-    
-        switch(event.keyCode){
+
+        switch (event.keyCode) {
             case 97: // LEFT
                 tankMoveIntity.moveLeft();
                 break;
@@ -148,14 +196,14 @@ function listenKeyboard(tankMoveIntity){
 
 
 // 获取像素点的rgba值
-function getPixelRgba(x,y,width=2,height=2){
+function getPixelRgba(x, y, width = 30, height = 30) {
     // 获取画布
     var canvasObj = $("#map")[0].getContext('2d');
 
     //
-    var pixelData = canvasObj.getImageData(x,y,width,height).data;
+    var pixelData = canvasObj.getImageData(x, y, width, height).data;
     var rgba = 'rgba(' + pixelData[0] + ',' + pixelData[1] +
-                ',' + pixelData[2] + ',' + (pixelData[3] / 255) + ')';
+        ',' + pixelData[2] + ',' + (pixelData[3] / 255) + ')';
 
     return rgba;
 }
